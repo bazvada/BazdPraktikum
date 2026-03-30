90cf0bd5fac19f507b67f2665657f50f522596c7
bazvada <bazdyrevadara0@gmail.com>
2026-03-30 21:27:33 +0300

    bazdD

---
+++ blogicum/blog/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from .models import Post, Comment, User
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'text',
+            'pub_date',
+            'image',
+            'location',
+            'category',
+            'is_published',
+        )
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('text',)
+
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
